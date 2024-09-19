ee2984d7e8cf2f1079e5fb2e7471d2db562e0b90
niculuka <niculuka@yahoo.com>
2024-09-19 17:54:10 +0300

    updtes 2

--- src/app/guest/weather-five/weather-five.component.ts
+++ src/app/guest/weather-five/weather-five.component.ts
@@ -4,6 +4,7 @@ import { Weather } from '../../shared/model/weather.model';
 import { CurrentLocationService } from 'src/app/shared/services/current-location.service';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { WallpaperService } from 'src/app/shared/services/wallpaper.service';
+import { FavoriteService } from 'src/app/shared/services/favorite.service';
 
 @Component({
   selector: 'weather-five',
@@ -17,26 +18,34 @@ export class WeatherFiveComponent implements OnInit, OnDestroy {
   currentCity: any = "";
   searchCity: string = "";
 
+  favorites: Weather[] = [];
+
   private sub0: any;
   private sub1: any;
   private sub2: any;
+  private sub3: any;
 
   constructor(
     private currLocationService: CurrentLocationService,
     private weatherService: WeatherService,
     private wallpaperService: WallpaperService,
+    private favoriteService: FavoriteService,
     private matSnackBar: MatSnackBar
-  ) { }
+  ) {
+    this.sub0 = favoriteService.getFavoritesObservable().subscribe(data => {
+      this.favorites = data;
+    });
+  }
 
   ngOnInit(): void {
-    this.sub0 = this.currLocationService.getCurrentLocationObservable().subscribe(data => {
+    this.sub1 = this.currLocationService.getCurrentLocationObservable().subscribe(data => {
       this.myWeather5.lat = data.lat;
       this.myWeather5.lon = data.lon;
       if (this.myWeather5.lat == 0 || this.myWeather5.lon == 0) {
         this.currLocationService.getLocationService();
       }
       else {
-        this.sub1 = this.currLocationService.getCurrentCityService(this.myWeather5.lat, this.myWeather5.lon).subscribe({
+        this.sub2 = this.currLocationService.getCurrentCityService(this.myWeather5.lat, this.myWeather5.lon).subscribe({
           next: (data) => {
             this.currentCity = data.name;
             this.get5DaysWeather(data.name);
@@ -52,7 +61,7 @@ export class WeatherFiveComponent implements OnInit, OnDestroy {
 
   // WEATHER 5 --------------------------------------------------------------------------
   get5DaysWeather(city: string) {
-    this.sub2 = this.weatherService.get5DaysWeatherService(city).subscribe({
+    this.sub3 = this.weatherService.get5DaysWeatherService(city).subscribe({
       next: (data: any) => {
         this.myWeather5.currentCity = data.city.name;
         this.myWeather5.list = data.list;
@@ -76,10 +85,30 @@ export class WeatherFiveComponent implements OnInit, OnDestroy {
     }
   }
 
+  // FAVORITES ----------------------------------------------------------------------------
+  isFavorite() {
+    let city = this.favorites.find(item => item.currentCity == this.myWeather5.currentCity);
+    if (city) {
+      this.myWeather5.favorite = true;
+    } else {
+      this.myWeather5.favorite = false;
+    }
+  }
+
+  getFavoriteCity(item: Weather) {
+    this.get5DaysWeather(item.currentCity);
+  }
+
+  removeFavoriteCity(item: Weather) {
+    this.favoriteService.removeFavoritesService(item);
+    this.isFavorite();
+  }
+
   // DESTROY ----------------------------------------------------------------------------
   ngOnDestroy(): void {
     this.sub0?.unsubscribe();
     this.sub1?.unsubscribe();
     this.sub2?.unsubscribe();
+    this.sub3?.unsubscribe();
   }
 }
