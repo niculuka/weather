826af94a5f8ebe9244afe91fdbaad91053a7e1b1
niculuka <niculuka@yahoo.com>
2024-09-19 20:50:58 +0300

    update 3

--- src/app/shared/services/wallpaper.service.ts
+++ src/app/shared/services/wallpaper.service.ts
@@ -23,11 +23,35 @@ export class WallpaperService {
                 break;
             case "11d": this.wallpaper = "assets/images/day11.jpg";
                 break;
+
+            case "12d": this.wallpaper = "assets/images/day11.jpg";
+                break;
+
             case "13d": this.wallpaper = "assets/images/day13.jpg";
                 break;
             case "50d": this.wallpaper = "assets/images/day50.jpg";
                 break;
+
             case "01n": this.wallpaper = "assets/images/night.jpg";
+                break;
+            case "02n": this.wallpaper = "assets/images/night.jpg";
+                break;
+            case "03n": this.wallpaper = "assets/images/night.jpg";
+                break;
+            case "04n": this.wallpaper = "assets/images/night.jpg";
+                break;
+
+            case "09n": this.wallpaper = "assets/images/night.jpg";
+                break;
+            case "10n": this.wallpaper = "assets/images/night.jpg";
+                break;
+            case "11n": this.wallpaper = "assets/images/night.jpg";
+                break;
+            case "12n": this.wallpaper = "assets/images/night.jpg";
+                break;
+            case "13n": this.wallpaper = "assets/images/night.jpg";
+                break;
+            case "50d": this.wallpaper = "assets/images/day50.jpg";
                 break;
             default:
                 this.wallpaper = "assets/images/day.jpg";
