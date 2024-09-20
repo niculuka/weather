79a1e3d6e2885de072c8b03bc4ea3839e0a8a4aa
niculuka <niculuka@yahoo.com>
2024-09-20 10:09:00 +0300

    almost done

--- src/app/shared/services/wallpaper.service.ts
+++ src/app/shared/services/wallpaper.service.ts
@@ -23,35 +23,31 @@ export class WallpaperService {
                 break;
             case "11d": this.wallpaper = "assets/images/day11.jpg";
                 break;
-
-            case "12d": this.wallpaper = "assets/images/day11.jpg";
+            case "12d": this.wallpaper = "assets/images/day12.jpg";
                 break;
-
             case "13d": this.wallpaper = "assets/images/day13.jpg";
                 break;
             case "50d": this.wallpaper = "assets/images/day50.jpg";
                 break;
-
-            case "01n": this.wallpaper = "assets/images/night.jpg";
+            case "01n": this.wallpaper = "assets/images/night01.jpg";
                 break;
-            case "02n": this.wallpaper = "assets/images/night.jpg";
+            case "02n": this.wallpaper = "assets/images/night02.jpg";
                 break;
-            case "03n": this.wallpaper = "assets/images/night.jpg";
+            case "03n": this.wallpaper = "assets/images/night03.jpg";
                 break;
-            case "04n": this.wallpaper = "assets/images/night.jpg";
+            case "04n": this.wallpaper = "assets/images/night04.jpg";
                 break;
-
-            case "09n": this.wallpaper = "assets/images/night.jpg";
+            case "09n": this.wallpaper = "assets/images/night09.jpg";
                 break;
-            case "10n": this.wallpaper = "assets/images/night.jpg";
+            case "10n": this.wallpaper = "assets/images/night10.jpg";
                 break;
-            case "11n": this.wallpaper = "assets/images/night.jpg";
+            case "11n": this.wallpaper = "assets/images/night11.jpg";
                 break;
-            case "12n": this.wallpaper = "assets/images/night.jpg";
+            case "12n": this.wallpaper = "assets/images/night12.jpg";
                 break;
-            case "13n": this.wallpaper = "assets/images/night.jpg";
+            case "13n": this.wallpaper = "assets/images/night13.jpg";
                 break;
-            case "50d": this.wallpaper = "assets/images/day50.jpg";
+            case "50d": this.wallpaper = "assets/images/night50.jpg";
                 break;
             default:
                 this.wallpaper = "assets/images/day.jpg";
